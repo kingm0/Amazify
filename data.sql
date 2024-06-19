4cb3312f49cd306ba365b56770aa59beee6e6a2e
kingm0 <124125722+kingm0@users.noreply.github.com>
2024-06-19 19:10:43 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,30 +1,3 @@
-
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `zip` varchar(20) NOT NULL,
-  `card_name` varchar(255) NOT NULL,
-  `card_number` varchar(20) NOT NULL,
-  `exp_month` varchar(10) NOT NULL,
-  `exp_year` varchar(10) NOT NULL,
-  `cvv` varchar(10) NOT NULL,
-  `order_date` timestamp NOT NULL DEFAULT current_timestamp()
-) 
-
-INSERT INTO `orders` (`order_id`, `full_name`, `email`, `address`, `city`, `state`, `zip`, `card_name`, `card_number`, `exp_month`, `exp_year`, `cvv`, `order_date`) VALUES
-(1, 'Mukul Singhal', 'singhalmukul920@gmail.com', 'nishank nagar  colony', 'Agra', 'Uttar Pradesh', '283121', 'Mukul singhal', '64984654984616', '1', '2028', '352', '2024-04-22 01:22:41'),
-(6, 'Ria chugh', 'riachugh010903@gmail.com', 'dadjfoi', 'difoah', 'dfkjsgioash', 'dofha', 'dfjnaoih', 'sdfhao', 'dnfaoih', 'cdhfoaish', 'fadsoifh', '2024-04-23 11:39:33');
-
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
-
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
