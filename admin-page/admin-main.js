b98c57a1bb2de7addf751794135a84febb91b008
Agrim Nauriyal <agrim.nauriyal@gmail.com>
2024-06-21 13:55:13 +0530

    add code for delete button

---
+++ admin-page/admin-main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteButtons = document.querySelectorAll('.delete-button');
+
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const card = button.closest('.card');
+            card.remove();
+        });
+    });
+});
