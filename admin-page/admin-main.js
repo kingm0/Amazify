4cf2f636056107f319cf843147084b3a7f5aa855
Agrim Nauriyal <agrim.nauriyal@gmail.com>
2024-06-22 10:39:55 +0530

    Create function to add cards

--- admin-page/admin-main.js
+++ admin-page/admin-main.js
@@ -8,3 +8,63 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+const productsSection = document.querySelector('.products');
+
+function createProductCard(title, price, description, imageData) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const imagePlaceholder = document.createElement('div');
+    imagePlaceholder.classList.add('image-placeholder');
+    const img = document.createElement('img');
+    img.width = 150;
+    img.height = 150;
+    img.src = imageData || "https://via.placeholder.com/150";
+    img.alt = "Image Placeholder";
+    imagePlaceholder.appendChild(img);
+
+    const information = document.createElement('div');
+    information.classList.add('information');
+
+    const productTitle = document.createElement('h2');
+    productTitle.classList.add('product-title');
+    productTitle.textContent = title;
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('product-price');
+    productPrice.textContent = `₹ ${price}`;
+    const productDescription = document.createElement('p');
+    productDescription.classList.add('product-description');
+    productDescription.textContent = description;
+
+    const productStatus = document.createElement('p');
+    productStatus.classList.add('product-status', 'product-rejected');
+    productStatus.textContent = "Rejected";
+
+    information.appendChild(productTitle);
+    information.appendChild(productPrice);
+    information.appendChild(productDescription);
+    information.appendChild(productStatus);
+
+    const deleteButton = document.createElement('img');
+    deleteButton.classList.add('delete-button');
+    deleteButton.src = "./svg/delete-outline.svg";
+    deleteButton.alt = "Delete Button";
+    deleteButton.addEventListener('click', function() {
+        const card = deleteButton.closest('.card');
+        card.remove();
+    });
+
+    card.appendChild(imagePlaceholder);
+    card.appendChild(information);
+    card.appendChild(deleteButton);
+
+    productsSection.appendChild(card); 
+}
+
+createProductCard(
+    'Samsung Galaxy S21 (128 GB) - Black', 
+    '56,999', 
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, corporis autem! Exercitationem magnam obcaecati, iusto architecto, veritatis reiciendis suscipit sequi delectus, maxime vitae illo hic!',
+    'https://via.placeholder.com/150'
+);
